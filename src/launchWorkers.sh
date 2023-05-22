b38e63805676ef61a455697a22f9b90d5d989f43
dianarrmiranda <dianarrmiranda@ua.pt>
2023-05-22 22:33:05 +0100

    ficheiro bash para lançar workers

---
+++ src/launchWorkers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NUM_WORKERS=5
+
+gnome-terminal --tab --title="Worker 1" -- python3 worker.py
+
+for ((i=2; i<=NUM_WORKERS; i++)); do
+    gnome-terminal --tab --title="Worker $i" -- python3 worker.py
+done
+
+wait
