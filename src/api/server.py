eb01698313c23076e9ab07830255758e21daadae
dianarrmiranda <dianarrmiranda@ua.pt>
2023-06-07 23:20:56 +0100

    tentativa de colocar o reset a apagar as filas falhada

--- src/api/server.py
+++ src/api/server.py
@@ -1,15 +1,11 @@
-import functools
 from math import ceil
 import os
 import time
-from schemas import Music
 import pika
-import signal, sys
 from pydub import AudioSegment
 import json
 from io import BytesIO
 import threading
-from pika import heartbeat
 
 class Server(threading.Thread):
 
@@ -20,38 +16,29 @@ class Server(threading.Thread):
         self.daemon = True
         self.isRunning = True
 
-        # Músicas a serem processadas
         self.musicData = []
-        # Partes processadas
         self.processedParts = {} # {musicID: {instrumento: {part_index: audio_part}}}
+        self.num_parts = {} # musicID -> num de partes
+        self.tracks = {} # musicID -> [instrumentos]
+        self.startTime = {} #musicId - {part_index: time}
+        self.time = {} #musicId - {part_index: time}
+        self.size = {} #musicId - {part_index: size}
+        self.jobslist = {} #nJob -> {part_index -> [size, time, music_id, track_id[tracks] ]}
+        self.nJob = -1
 
+        self.progress = {} #musicID -> [progress, [instruments], final]
+        self.jobProgress = {} #musicId -> numero de partes recebidas
+    
         # Rabbit MQ - Enviar não processadas
         self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=60, blocked_connection_timeout=10))
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue="music_parts")
-
         # Rabbit MQ - Receber processadas
         self.channel.queue_declare(queue="processed_parts")
         self.channel.basic_qos(prefetch_count=1)
         self.channel.basic_consume(queue="processed_parts", on_message_callback=self.receive_music_parts)
 
-        self.num_parts = {} # musicID -> num de partes
-        self.tracks = {} # musicID -> [instrumentos]
 
-        self.startTime = {} #musicId - {part_index: time}
-        self.time = {} #musicId - {part_index: time}
-
-        self.size = {} #musicId - {part_index: size}
-
-        self.jobslist = {} #nJob -> {part_index -> [size, time, music_id, track_id[tracks] ]}
-        self.nJob = -1
-
-        self.controlReceived = {} #musicId -> [part_index]
-
-        self.progress = {} #musicID -> [progress, [instruments], final]
-        self.jobProgress = {} #musicId -> numero de partes recebidas
-
-        self.instRecevided = {} #musicId -> [instruments]
 
     def run(self):
         while self.isRunning:
@@ -62,19 +49,15 @@ class Server(threading.Thread):
         print(' [*] Stopping server...')
         self.isRunning = False
         self.connection.process_data_events(time_limit=1)
-
-        dir = "static/unprocessed"
-        for f in os.listdir(dir):
-            os.remove(os.path.join(dir, f))
-
-        dir = "static/processed"
-        for f in os.listdir(dir):
-            os.remove(os.path.join(dir, f)) 
+        self.clear_directory("static/unprocessed")
+        self.clear_directory("static/processed")
         if self.connection.is_open:
             self.connection.close()
-
         print(' [*] Server stopped')
-    
+
+    def clear_directory(self, directory):
+        for f in os.listdir(directory):
+            os.remove(os.path.join(directory, f))
 
     def send_music_part(self, part_data):
         self.channel.basic_publish(
@@ -90,13 +73,10 @@ class Server(threading.Thread):
         self.time[music.music_id] = {}
         self.size[music.music_id] = {}
         self.progress[music.music_id] = [0, [], '']
-        self.instRecevided[music.music_id] = []
         
-    
     def getMusic(self, music_id):
         return self.musicData[music_id]
     
-
     def listAll(self):
         return self.musicData
     
@@ -108,8 +88,8 @@ class Server(threading.Thread):
         self.nJob += 1
         self.jobslist[self.nJob] = {} 
         self.tracks[music_id] = tracks_names.copy()
-        self.controlReceived[music_id] = []
         self.jobProgress[music_id] = 0
+
         music = self.getMusic(music_id)
 
         # Carregar o arquivo de áudio
@@ -131,7 +111,6 @@ class Server(threading.Thread):
                 self.jobProgress[music_id] += 1
                 tracks_names.remove(track)  # Remove o instrumento da lista de instrumentos a serem processados
 
-
         self.jobProgress[music_id] *= self.num_parts[music_id]
         self.progress[music_id][0] = (100*self.jobProgress[music_id])/(self.num_parts[music_id] * len(self.tracks[music_id]))
 
@@ -149,19 +128,17 @@ class Server(threading.Thread):
 
                 self.startTime[music_id][i] = time.time()
                 self.size[music_id][i] = len(part_data['part_audio'])
-
                 self.connection.add_callback_threadsafe(lambda: self.send_music_part(part_data))
         else:
             self.joinInstruments(music_id)
 
-    def receive_music_parts(self, ch, method, properties, body):        
+    def receive_music_parts(self, ch, method, properties, body):
         part_data = json.loads(body)
         music_id = part_data['music_id']
         part_index = part_data['part_index']
         part_audio = part_data['part_audio'].encode('latin1')  # Converte a string em bytes
         instrument = part_data['instrument']
 
-        
         if(self.jobProgress[music_id] < self.num_parts[music_id]*len(self.tracks[music_id])):
             self.jobProgress[music_id] +=1 
         
@@ -169,11 +146,7 @@ class Server(threading.Thread):
 
         self.time[music_id][part_index] = time.time() - self.startTime[music_id][part_index]
 
-        if music_id not in self.controlReceived:
-            self.controlReceived[music_id] = []
-        
-        if part_index not in self.controlReceived[music_id]:
-            self.controlReceived[music_id].append(part_index)
+        if part_index not in self.jobslist[self.nJob]:
             self.jobslist[self.nJob][part_index] = [self.size[music_id][part_index], self.time[music_id][part_index], music_id, [instrument]]
         else:
             inst = self.jobslist[self.nJob][part_index][-1]
@@ -203,8 +176,6 @@ class Server(threading.Thread):
         joinedParts.export("static/processed/" + str(music_id) + "_" + instrument + ".wav", format="wav")
 
         self.progress[music_id][1].append([instrument, '/static/processed/{}_{}.wav'.format(music_id, instrument)])
-
-        self.instRecevided[music_id].append(instrument)
 
         if(self.jobProgress[music_id]/self.num_parts[music_id]) == len(self.tracks[music_id]):
             self.joinInstruments(music_id)
@@ -238,30 +209,22 @@ class Server(threading.Thread):
                 count += 1
 
         return allJobs
-
+    
 
     def reset(self):
-
-        self.workerStatus = {} 
         self.jobslist = {}
         self.startTime = {}
         self.time = {}
         self.size = {}
         self.processedParts = {}
-        self.controlReceived = {}
         self.num_parts = {}
         self.nJob = -1
         self.musicData = []
-        self.ctrWork = -1
+        self.tracks = {}
+        self.jobProgress = {}
+        self.progress = {}
 
-        dir = "static/unprocessed"
-        for f in os.listdir(dir):
-            os.remove(os.path.join(dir, f))
+        self.clear_directory('static/processed')
+        self.clear_directory('static/unprocessed')
 
-        dir = "static/processed"
-        for f in os.listdir(dir):
-            os.remove(os.path.join(dir, f)) 
-
-        if self.connection.is_open:
-            self.channel.queue_purge(queue="music_parts")
-            self.channel.queue_purge(queue="processed_parts")
+        print(' [*] Reseted all data')
